1b41113b6e8c4670621e830579a5c70945a2b9ff
Ryan <quan0203@gmail.com>
2018-09-01 21:00:28 +0800

    initial commit for abp aspnetcore angular

---
+++ Abp AspNetCore Angular EF6/README.md
@@ -0,0 +1,2 @@
+# Abp AspNetCore Angular EF6
+Base on Abp AspNetCorew Angular 4.0.1
---
+++ Abp AspNetCore Angular EF6/aspnet-core/src/MyDemo.MyProject.Core/Configuration/AppSettingNames.cs
@@ -0,0 +1,7 @@
+﻿namespace MyDemo.MyProject.Configuration
+{
+    public static class AppSettingNames
+    {
+        public const string UiTheme = "App.UiTheme";
+    }
+}
