8ad88269d995711ba686cb85ffcfb6d37910dcb2
ryancyq <ryancyq@gmail.com>
2018-10-23 01:56:37 +0800

    Update Test Project

--- jquery/4.2.0/aspnet-core/test/MyDemo.MyProject.Tests/MyProjectTestBase.cs
+++ jquery/4.2.0/aspnet-core/test/MyDemo.MyProject.Tests/MyProjectTestBase.cs
@@ -1,24 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Abp;
 using Abp.Authorization.Users;
+using Abp.Domain.Uow;
 using Abp.Events.Bus;
 using Abp.Events.Bus.Entities;
 using Abp.MultiTenancy;
 using Abp.Runtime.Session;
 using Abp.TestBase;
+using Castle.MicroKernel.Registration;
+using Effort;
 using MyDemo.MyProject.Authorization.Users;
-using MyDemo.MyProject.EntityFrameworkCore;
-using MyDemo.MyProject.EntityFrameworkCore.Seed.Host;
-using MyDemo.MyProject.EntityFrameworkCore.Seed.Tenants;
+using MyDemo.MyProject.EntityFramework;
+using MyDemo.MyProject.EntityFramework.Seed.Host;
+using MyDemo.MyProject.EntityFramework.Seed.Tenants;
 using MyDemo.MyProject.MultiTenancy;
 
 namespace MyDemo.MyProject.Tests
 {
     public abstract class MyProjectTestBase : AbpIntegratedTestBase<MyProjectTestModule>
     {
+        private DbConnection _hostDb;
+        private Dictionary<int, DbConnection> _tenantDbs; //only used for db per tenant architecture
+
         protected MyProjectTestBase()
         {
             void NormalizeDbContext(MyProjectDbContext context)
@@ -28,7 +35,7 @@ namespace MyDemo.MyProject.Tests
                 context.SuppressAutoSetTenantId = true;
             }
 
-            // Seed initial data for host
+            //Seed initial data for host
             AbpSession.TenantId = null;
             UsingDbContext(context =>
             {
@@ -37,7 +44,7 @@ namespace MyDemo.MyProject.Tests
                 new DefaultTenantBuilder(context).Create();
             });
 
-            // Seed initial data for default tenant
+            //Seed initial data for default tenant
             AbpSession.TenantId = 1;
             UsingDbContext(context =>
             {
@@ -46,6 +53,64 @@ namespace MyDemo.MyProject.Tests
             });
 
             LoginAsDefaultTenantAdmin();
+        }
+
+        protected override void PreInitialize()
+        {
+            base.PreInitialize();
+
+            /* You can switch database architecture here: */
+            UseSingleDatabase();
+            //UseDatabasePerTenant();
+        }
+
+        /* Uses single database for host and all tenants.
+         */
+        private void UseSingleDatabase()
+        {
+            _hostDb = DbConnectionFactory.CreateTransient();
+
+            LocalIocManager.IocContainer.Register(
+                Component.For<DbConnection>()
+                    .UsingFactoryMethod(() => _hostDb)
+                    .LifestyleSingleton()
+                );
+        }
+
+        /* Uses single database for host and Default tenant,
+         * but dedicated databases for all other tenants.
+         */
+        private void UseDatabasePerTenant()
+        {
+            _hostDb = DbConnectionFactory.CreateTransient();
+            _tenantDbs = new Dictionary<int, DbConnection>();
+
+            LocalIocManager.IocContainer.Register(
+                Component.For<DbConnection>()
+                    .UsingFactoryMethod((kernel) =>
+                    {
+                        lock (_tenantDbs)
+                        {
+                            var currentUow = kernel.Resolve<ICurrentUnitOfWorkProvider>().Current;
+                            var abpSession = kernel.Resolve<IAbpSession>();
+
+                            var tenantId = currentUow != null ? currentUow.GetTenantId() : abpSession.TenantId;
+
+                            if (tenantId == null || tenantId == 1) //host and default tenant are stored in host db
+                            {
+                                return _hostDb;
+                            }
+
+                            if (!_tenantDbs.ContainsKey(tenantId.Value))
+                            {
+                                _tenantDbs[tenantId.Value] = DbConnectionFactory.CreateTransient();
+                            }
+
+                            return _tenantDbs[tenantId.Value];
+                        }
+                    }, true)
+                    .LifestyleTransient()
+                );
         }
 
         #region UsingDbContext
@@ -191,20 +256,20 @@ namespace MyDemo.MyProject.Tests
         /// Gets current user if <see cref="IAbpSession.UserId"/> is not null.
         /// Throws exception if it's null.
         /// </summary>
-        protected async Task<User> GetCurrentUserAsync()
+        protected Task<User> GetCurrentUserAsync()
         {
             var userId = AbpSession.GetUserId();
-            return await UsingDbContext(context => context.Users.SingleAsync(u => u.Id == userId));
+            return Task.FromResult(UsingDbContext(context => context.Users.Single(u => u.Id == userId)));
         }
 
         /// <summary>
         /// Gets current tenant if <see cref="IAbpSession.TenantId"/> is not null.
         /// Throws exception if there is no current tenant.
         /// </summary>
-        protected async Task<Tenant> GetCurrentTenantAsync()
+        protected Task<Tenant> GetCurrentTenantAsync()
         {
             var tenantId = AbpSession.GetTenantId();
-            return await UsingDbContext(context => context.Tenants.SingleAsync(t => t.Id == tenantId));
+            return Task.FromResult(UsingDbContext(context => context.Tenants.Single(t => t.Id == tenantId)));
         }
     }
 }
